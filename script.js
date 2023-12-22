3a1ac88298e224b05eebf03dbe7971dda79c6605
loki <uddeeptaraajkashyap@gmail.com>
2023-12-22 09:30:51 +0530

    update

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-const takePhoto = require('./takePhoto');
-const uploadPhoto = require('./uploadPhoto');
+const { exec } = require('child_process');
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
 const axios = require('axios');
 
-const takePhotoAndUpload = async () => {
-    takePhoto(async (filename) => {
-        const url = await uploadPhoto(filename);
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
-        if (url) {
-            const data = {
-                oreGrade: 'A',
-                silicon: 0.5,
-                iron: 0.5,
-            };
-
-            try {
-                const response = await axios.post('https://oresense.onrender.com/data/photo', {
-                    "ml_detail": JSON.stringify(data),
-                    "image_url": url
-                });
-                console.log(response.data);
-            } catch (error) {
-                console.error(`Error posting data: ${error.message}`);
+const takeAndUploadPhoto = async () => {
+    const command = `rpicam-jpeg -o photo.jpg -t 2000 --width 640 --height 480 --ev 0.5`;
+    await new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error taking photo: ${error.message}`);
+                reject(error);
             }
-        }
+            if (stderr) {
+                console.error(`Error: ${stderr}`);
+                reject(stderr);
+            }
+            console.log(`Photo taken and saved as photo.jpg`);
+            resolve();
+        });
     });
+
+    const result = await cloudinary.uploader.upload("photo.jpg");
+    console.log(result.url);
+
+    const data = {
+        oreGrade: 'A',
+        silicon: 0.5,
+        iron: 0.5,
+    };
+
+    const response = await axios.post('4.227.178.188:3001/data/photo', {
+        ml_detail: JSON.stringify(data),
+        image_url: result.url
+    });
+
+    console.log(response.data);
 };
 
-setInterval(takePhotoAndUpload, 10000)
+takeAndUploadPhoto();
